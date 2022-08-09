06c97eaba062b0edb2197578fc5d2f838882db3f
Ethansnow2012 <ethansnow2012@gmail.com>
2022-08-09 15:20:48 +0800

    feat: 3d restart trial

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,7 +3,7 @@ require("@rushstack/eslint-patch/modern-module-resolution");
 
 module.exports = {
   "root": true,
-  "extends": ["plugin:vue/vue3-essential", "eslint:recommended", "@vue/eslint-config-typescript/recommended", "@vue/eslint-config-prettier", "plugin:storybook/recommended"],
+  "extends": ["plugin:vue/vue3-essential", "eslint:recommended", "@vue/eslint-config-typescript/recommended", "plugin:storybook/recommended"],// "@vue/eslint-config-prettier",
   "overrides": [{
     "files": ["cypress/e2e/**.{cy,spec}.{js,ts,jsx,tsx}"],
     "extends": ["plugin:cypress/recommended"]
